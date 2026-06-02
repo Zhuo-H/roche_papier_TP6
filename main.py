3e6a9c99749dfbc47a362c6e47c9a8fe05d6a02d
hanz <hanz@educ.college-montreal.qc.ca>
2026-06-02 16:19:09 -0400

    stuff

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+"""""
+Fait par: Zhuo Han
+Jeu de roche papier ciseau
+"""""
+
 import random
 
 import arcade
+from pyglet.event import EVENT_HANDLE_STATE
 
 from attack_animation import AttackType, AttackAnimations
 
 
 SCREEN_WIDTH = 1080
 SCREEN_HEIGHT = 900
-SCREEN_TITLE = "Rock Paper Cissors +"
+SCREEN_TITLE = "roche papier ciseau +"
 
 
 class MyGame(arcade.Window):
@@ -31,7 +37,7 @@ class MyGame(arcade.Window):
         self.scissors_open.position = (370, 270)
         self.rock_open = AttackAnimations(AttackType.ROCK)
         self.rock_open.position = (190, 270)
-        self.computer_3 = arcade.Sprite("images/computer_3.png" , 0.1)
+        self.computer_3 = arcade.Sprite("images/computer_3.png", 0.1)
         self.computer_3.position = (800, 400)
         self.computer_2 = arcade.Sprite("images/computer_2.png", 0.1)
         self.computer_2.position = (280, 400)
@@ -48,7 +54,7 @@ class MyGame(arcade.Window):
         self.computer_paper = AttackAnimations(AttackType.PAPER)
         self.computer_scissors = AttackAnimations(AttackType.SCISSORS)
 
-        self.computer_rock.position = (802,270)
+        self.computer_rock.position = (802, 270)
         self.computer_scissors.position = (802, 270)
         self.computer_paper.position = (802, 270)
 
@@ -67,8 +73,9 @@ class MyGame(arcade.Window):
         self.score_computer = 0
         self.hide_timer = 0
         self.can_click = True
-        self.show_computer_choice =True
+        self.show_computer_choice = True
         self.hidden_sprites = []
+        self.game_over = False
 
     def draw_square(self):
         arcade.draw_lrbt_rectangle_outline(180, 380, 350, 450,  arcade.color.RED, 3)
@@ -79,7 +86,7 @@ class MyGame(arcade.Window):
         arcade.draw_lrbt_rectangle_outline(330, 415, 230, 310, arcade.color.RED, 2.5)
         arcade.draw_lrbt_rectangle_outline(760, 845, 230, 310, arcade.color.RED, 2.5)
 
-    def Text(self):
+    def text(self):
         arcade.draw_text("Roche, Papier, Ciseau", 250, 800, arcade.color.WHITE, 50)
         arcade.draw_text("Un nouveau jeu original!", 250, 760, arcade.color.WHITE, 20)
 
@@ -89,21 +96,42 @@ class MyGame(arcade.Window):
         self.scissors_open_list.draw()
         self.doom_guy3_list.draw()
 
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.ESCAPE:
+            self.close()
+        if symbol == arcade.key.SPACE and self.game_over:
+            self.score = 0
+            self.score_computer = 0
+            self.result = ""
+
+            self.player_choice = ""
+            self.computer_choice = ""
+
+            self.game_over = False
+            self.can_click = True
+            self.hide_timer = 0
+
+            self.computer_rock.visible = True
+            self.computer_paper.visible = True
+            self.computer_scissors.visible = True
+
     def on_mouse_press(self, x, y, button, modifiers):
+        if self.game_over == True:
+            return
+
+
 
         if not self.can_click:
 
             return
 
-
-
-        if self.rock_open.collides_with_point((x,y)):
+        if self.rock_open.collides_with_point((x, y)):
             self.player_choice = 1
             self.hidden_sprites = [self.paper_open, self.scissors_open]
-        elif self.paper_open.collides_with_point((x,y)):
+        elif self.paper_open.collides_with_point((x, y)):
             self.player_choice = 2
             self.hidden_sprites = [self.rock_open, self.scissors_open]
-        elif self.scissors_open.collides_with_point((x,y)):
+        elif self.scissors_open.collides_with_point((x, y)):
             self.player_choice = 3
             self.hidden_sprites = [self.paper_open, self.rock_open]
         else:
@@ -127,28 +155,26 @@ class MyGame(arcade.Window):
 
 
 
-
         self.show_computer_choice = True
         self.hide_timer = 2
         self.can_click = False
         self.action()
-
-
-
 
     def action(self):
         p = self.player_choice
         c = self.computer_choice
 
         if (p == 1 and c == 2) or (p == 2 and c == 3) or (p == 3 and c == 1):
-            self.result = 'lose'
+            self.result = 'défaite'
             self.score_computer += 1
         elif (p == 2 and c == 1) or (p == 3 and c == 2) or (p == 1 and c == 3):
-            self.result = 'win'
+            self.result = 'victoire'
             self.score += 1
         else:
-            self.result = 'tie'
+            self.result = 'égalité'
 
+        if self.score >= 3 or self.score_computer >=3:
+            self.game_over = True
 
     def on_update(self, delta_time):
         self.scissors_open.on_update()
@@ -177,18 +203,26 @@ class MyGame(arcade.Window):
             self.computer_damage -= 1
             self.timer = 0
 
-
     def on_draw(self):
         self.clear()
         self.draw_square()
         self.rock_open_list.draw()
         self.computer_list3.draw()
-        self.Text()
+        self.text()
         self.draw_hands()
-        arcade.draw_text(f"the result is a: {self.result}", 250, 500, arcade.color.WHITE, 50)
-        arcade.draw_text(f"the computer score is: {self.score_computer}", 700, 200, arcade.color.WHITE, 20)
-        arcade.draw_text(f"your score is: {self.score}", 150, 200, arcade.color.WHITE, 20)
+        arcade.draw_text(f"Le résultat est: {self.result}", 250, 500, arcade.color.WHITE, 50)
+        arcade.draw_text(f"Le pointage de l'ordinateur est: {self.score_computer}", 700, 200, arcade.color.WHITE, 20)
+        arcade.draw_text(f"Votre pointage est: {self.score}", 150, 200, arcade.color.WHITE, 20)
         self.computer_choice_list.draw()
+
+        if self.game_over == True:
+            if self.score >= 3:
+                arcade.draw_text("VOOUS AVEZ GAGNÉ", 400, 600, arcade.color.GREEN, 40)
+            else:
+                arcade.draw_text("L'ORDINATEUR À GAGNÉ", 330, 600, arcade.color.RED)
+
+            arcade.draw_text("Appuyez 'ESPACE' pour recommencer", 300, 500, arcade.color.WHITE,25)
+            arcade.draw_text("Appuyer 'ESC' pour quitter", 340, 550, arcade.color.WHITE,25)
 
 
 def main():
